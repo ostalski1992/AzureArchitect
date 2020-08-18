df9fe60e476d58b88dcf7e3e9ec075c352c90287
ostalski1992 <61191778+ostalski1992@users.noreply.github.com>
2020-08-18 20:25:39 +0200

    Update ##DeployToAzure.ps1

--- Week3/3.4/##DeployToAzure.ps1
+++ Week3/3.4/##DeployToAzure.ps1
@@ -1,5 +1,8 @@
+Connect-AzAccount
+New-AzResourceGroup -Name 'rg-007-szkolachmury-dev-002' -Location "westeurope"
+
 New-AzResourceGroupDeployment -Name SzkolaChmury `
--ResourceGroupName "rg-007-szkolachmury-dev-001" `
+-ResourceGroupName "rg-007-szkolachmury-dev-002" `
 -TemplateUri "https://raw.githubusercontent.com/ostalski1992/AzureArchitect/master/Week3/3.4/00-linked.json" `
 -TemplateParameterUri "https://raw.githubusercontent.com/ostalski1992/AzureArchitect/master/Week3/3.4/zz-parameters.json"
 
